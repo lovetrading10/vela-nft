a9c7902e366f7d62ba928028698195eba469e8f1
Love Trading <lovetrading09@yahoo.com>
2023-02-17 13:50:01 -0600

    save cirrent progress

---
+++ tests/gravity.test.ts
@@ -0,0 +1,12 @@
+import { describe, test } from "matchstick-as/assembly/index"
+// import { handleNewGravatar } from "../../src/gravity"
+import {   
+    ClosePosition,
+    DecreasePosition,
+    IncreasePosition,
+    LiquidatePosition, } from "../generated/schema"
+describe("handleNewGravatar()", () => {
+  test("Should create a new Gravatar entity", () => {
+    // ...
+  })
+})
